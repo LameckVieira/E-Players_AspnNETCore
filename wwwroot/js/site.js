d72feb473a8077559b58e4c1c68209fd543f1a5c
FIC <FIC@info.local>
2021-01-15 10:30:37 -0300

    add css e html

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,12 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function botaoMenu(){
+    
+    var m = document.getElementById("menu")
+    if(m.style.display == "block"){
+        m.style.display = "none";
+    }else{
+        m.style.display = "block"
+    }
+}
